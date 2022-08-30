2609b384997f14aaa0aa9bf0f6fcec1903d0ab10
joaotavares42 <jvdst5@gmail.com>
2022-08-30 17:12:11 -0300

    create players method

---
+++ src/players/players.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreatePlayerDto} from './dtos/create-player.dto'
+import { PlayersService} from './players.service'
+
+@Controller('api/v1/players')
+export class PlayersController {
+
+    constructor(private readonly playersService: PlayersService) {}
+
+    @Post()
+    async createUpdatePlayer(
+        @Body() createPlayerDto: CreatePlayerDto) {
+    
+        await this.playersService.createUpdatePlayer(createPlayerDto)
+    }
+}
---
+++ src/players/players.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { CreatePlayerDto} from './dtos/create-player.dto';
+import { Player } from './interfaces/player.interface';
+import {v4 as uuidv4} from 'uuid'
+
+
+@Injectable()
+export class PlayersService {
+
+    private players: Player[] = [];
+
+    private readonly logger = new Logger(PlayersService.name)
+
+    async createUpdatePlayer(createPlayerDto: CreatePlayerDto) : Promise<void> {
+        
+
+        this.create(createPlayerDto)
+    
+    }
+
+    private create(createPlayerDto: CreatePlayerDto) : void {
+        const { name, phoneNumber, email } = createPlayerDto
+
+        const player: Player = {
+            _id: uuidv4(),
+            name,
+            email,
+            phoneNumber,
+            ranking: 'A',
+            rankingPosition: 1,
+            profilePictureUrl: 'www.google.com.br/picture123.jpg'
+        }
+        this.logger.log(`createPlayerDto: ${JSON.stringify(player)}`)
+        this.players.push(player)
+    }
+
+}
